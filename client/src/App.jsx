d9b0120ba2f730206d8b84ec148641e6e9791939
vaibhav <gholapvaibhav724@gmail.com>
2024-09-20 22:59:05 +0530

    Base Url Link Changed to Deployment Link

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import LeaderBoardPage from './page/LeaderBoardPage'
 import axios from 'axios'
 
 
-axios.defaults.baseURL ="http://localhost:5000"
+axios.defaults.baseURL ="https://threew-gto7.onrender.com"
 function App() {
 
   return (
